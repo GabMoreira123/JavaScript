95462f446aa9d2e86be1bcb6e041d093cb083ce5
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-09 14:10:22 -0400

    Arquivo de condicionais

---
+++ Estruturascondicionais/if.js
@@ -0,0 +1,7 @@
+let hora = 15;
+
+if (hora < 12) {
+    console.log("Bom dia!")
+}
+// O restante do código continua independentemente
+console.log("Dinheiro não traz felicidade. Me dê o seu e seja Feliz!");
