6708ec66f6e30be80ee0455c16c7aa87463614ff
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-14 13:47:15 -0400

    Arquivos sobre Js

---
+++ Funcoes/declaracao-funcao-com-parametros.js
@@ -0,0 +1,8 @@
+function nomeDaFuncao(parametro1, parametro2){
+    // Corpo da função 
+    //Pode conter uma ou mais instruções
+    return resultado; //Opcional
+}
+
+ //Chamada da Função
+ nomeDaFuncao(parametro1, parametro2);
