2d95a40b4d9b546c458ceaf72cea03fcac565f37
GabMoreira123 <gabrielsmoreira49@gmail.com>
2024-05-14 15:57:31 -0400

    Arquivo java função declarativa soma js

--- Funcoes/funcao-de-expressao-nomeada.js
+++ Funcoes/funcao-de-expressao-nomeada.js
@@ -2,6 +2,6 @@ const calcularQuadrado = function quadrado(numero){
    return numero * numero;
 
 };
-// Chamando a função para calcular de um, número
+// Chamando a função para calcular de um número
 const resultado = calcularQuadrado(5);
 console.log("Quadrado de 5:", resultado);
